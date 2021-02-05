61171375d6fc6481a194bbd65b66f93542cf06bf
Domes711 <48947330+Domes711@users.noreply.github.com>
2021-02-05 20:23:56 +0100

    Code review

--- eshop/Areas/Admin/Controllers/CarouselController.cs
+++ eshop/Areas/Admin/Controllers/CarouselController.cs
@@ -17,13 +17,13 @@ namespace eshop.Areas.Admin.Controllers
     [Authorize(Roles = nameof(Roles.Admin) + ", " + nameof(Roles.Manager))]
     public class CarouselController : Controller
     {
-        IHostingEnvironment env;
-        EshopDBContext EshopDBContext;
+        IHostingEnvironment env; //TODO - má to být _env, pridat readonly a private 
+        EshopDBContext EshopDBContext; //TODO - to same a zacinas nalým, pridat readonly a private 
 
         public CarouselController(EshopDBContext eshopDBContext, IHostingEnvironment env)
         {
             this.EshopDBContext = eshopDBContext;
-            this.env = env;
+            this.env = env; //TODO - this se moc nepouziva spis _ na private class
         }
 
         public async Task<IActionResult> Select()
@@ -41,14 +41,14 @@ namespace eshop.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(Carousel carousel)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) //TODO - u malych scoups je lepsi if(neco v pici) retrun kdyz je neco v pici
             {
                 var fuh = new FileUploadHelper(env.WebRootPath, "Carousels", "image");
                 carousel.ImageSrc = await fuh.UploadFileAsync(carousel.Image);
 
                 EshopDBContext.Add(carousel);
                 await EshopDBContext.SaveChangesAsync();
-                return RedirectToAction(nameof(Select));
+                return RedirectToAction(nameof(Select)); //TODO - nameof je top redirectToAction je celkem shit ale proc ne
             }
             else
             {
@@ -57,8 +57,8 @@ namespace eshop.Areas.Admin.Controllers
         }
 
         public IActionResult Edit(int id)
-        {
-            Carousel selectedCarousel = EshopDBContext.Carousels.Where(x => x.ID == id).FirstOrDefault();
+        {//TODO - radsi var
+            Carousel selectedCarousel = EshopDBContext.Carousels.Where(x => x.ID == id).FirstOrDefault(); //TODO - da se i psat FirstOrDefault(x => x.ID == id)
             if (selectedCarousel != null) return View(selectedCarousel);
             else return NotFound();
         }
@@ -66,10 +66,10 @@ namespace eshop.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> Edit(Carousel carousel)
         {
-            Carousel selectedCarousel = EshopDBContext.Carousels.Where(x => x.ID == carousel.ID).FirstOrDefault();
+            Carousel selectedCarousel = EshopDBContext.Carousels.Where(x => x.ID == carousel.ID).FirstOrDefault(); //TODO - to same
             if (ModelState.IsValid) 
             {
-                selectedCarousel.DataTarget = carousel.DataTarget;
+                selectedCarousel.DataTarget = carousel.DataTarget; //TODO - automapper nebo metoda map selected carousel ktera bere carousel
                 selectedCarousel.ImageAlt = carousel.ImageAlt;
                 selectedCarousel.CarouselContent = carousel.CarouselContent;
 
